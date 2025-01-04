d390bed250e243520a9595d0a9c0ab2e6197ba1a
Eljan Najafov <eljan.najafov02@mail.ru>
2025-01-04 20:33:00 +0400

    with out sec

---
+++ secsection1/src/main/java/com/sec/secsection1/Secsection1Application.java
@@ -0,0 +1,13 @@
+package com.sec.secsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secsection1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Secsection1Application.class, args);
+    }
+
+}
