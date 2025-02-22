6c15aaf4eac2d5d51edc3750b928946ffc67e0ba
Eljan Najafov <eljan.najafov02@mail.ru>
2025-02-22 13:04:41 +0400

    Accepting only HTTPS Traffic using Spring Security

---
+++ secsection7/src/main/java/com/sec/secsection7/Secsection7Application.java
@@ -0,0 +1,13 @@
+package com.sec.secsection4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secsection7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Secsection7Application.class, args);
+    }
+
+}
